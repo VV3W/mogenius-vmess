527e6ffb8075f2a94fe6ce3b13f1b4c4017454cf
VV3W <99074884+VV3W@users.noreply.github.com>
2022-10-01 14:55:56 +0800

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# V2Ray new configuration
+# Run V2ray
+# Write V2Ray configuration
+UUID=37c34bc3-5d3c-4102-b03c-ca405bcc9e06
+PATHM=xxp0606
+cat << EOF > /etc/v2ray/config.json
+{
+    "inbounds": [{
+        "port": 8080,
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "$UUID",
+                "alterId": 0
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "/$PATHM"
+            }
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
+#Run V2ray
+
+/usr/bin/v2ray/v2ray  run -config=/etc/v2ray/config.json
